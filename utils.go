620a918d7a4ac7eef37bf68d69949865abc4c838
boyu <boyu@freewheel.tv>
2018-04-28 15:29:20 +0800

    update

---
+++ utils.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type StrFlags []string
+
+func (s *StrFlags) String() string {
+	return fmt.Sprintf("%s", *s)
+}
+
+func (s *StrFlags) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+// __FILE__ returns the file name in which the function was invoked
+func FILE() string {
+	_, file, _, _ := runtime.Caller(1)
+	return file
+}
+
+// __LINE__ returns the line number at which the function was invoked
+func LINE() int {
+	_, _, line, _ := runtime.Caller(1)
+	return line
+}
+
+// GetFileModTime returns unix timestamp of `os.File.ModTime` for the given path.
+func GetFileModTime(path string) int64 {
+	path = strings.Replace(path, "\\", "/", -1)
+	f, err := os.Open(path)
+	if err != nil {
+		glog.Errorf("Failed to open file on '%s': %s", path, err)
+		return time.Now().Unix()
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		glog.Errorf("Failed to get file stats: %s", err)
+		return time.Now().Unix()
+	}
+
+	return fi.ModTime().Unix()
+}
